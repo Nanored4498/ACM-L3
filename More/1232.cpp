11ac7e7b65659d46d68bc7f05daaff34a73a4e0b
Yoann Coudert--Osmont <nanored.mc@gmail.com>
2019-09-01 18:00:09 +0200

    skyline

--- More/1232.cpp
+++ More/1232.cpp
@@ -3,67 +3,52 @@
 #include <algorithm>
 
 using namespace std;
+#define MAX 100000
 
-struct Build { int l, r, h, i; };
-bool compHigh(Build &a, Build &b) { return (a.h > b.h || (a.h == b.h && a.i > b.i)); }
-struct Pos { int x; bool deb; };
-bool compPos(Pos &a, Pos &b) { return a.x < b.x; }
+int mi[3*MAX], ma[3*MAX];
+
+int add(int l, int r, int h, int i, int a, int b) {
+	if(l >= b || r <= a) return 0;
+	if(h < mi[i]) return 0;
+	if(l <= a && b <= r) {
+		if(h < ma[i]) {
+			int m = (a+b)/2;
+			int res = add(l, r, h, 2*i, a, m) + add(l, r, h, 2*i+1, m, b);
+			mi[i] = min(mi[2*i], mi[2*i+1]);
+			return res;
+		} else {
+			mi[i] = ma[i] = h;
+			return b - a;
+		}
+	} else {
+		if(b > a+1 && mi[i] == ma[i])
+			mi[2*i] = ma[2*i] = mi[2*i+1] = ma[2*i+1] = mi[i];
+		ma[i] = max(ma[i], h);
+		int m = (a+b)/2;
+		int res = add(l, r, h, 2*i, a, m) + add(l, r, h, 2*i+1, m, b);
+		mi[i] = min(mi[2*i], mi[2*i+1]);
+		return res;
+	}
+}
 
 int main() {
 	cout.tie(NULL);
 	ios::sync_with_stdio(false);
 
-	int c, n;
-	int l, r, h;
-	int i;
+	int c;
 	cin >> c;
-	vector<Build> bs;
-	vector<vector<Pos>> lp;
-	vector<int> ts;
+	int n, l, r, h;
 	while(c --> 0) {
 		cin >> n;
-		bs.clear();
-		lp.clear();
-		ts.clear();
-		for(i = 1; i <= n; i++) {
-			cin >> l >> r >> h;
-			bs.push_back({l, r, h, i});
-		}
-		sort(bs.begin(), bs.end(), compHigh);
-		ts.push_back(bs[0].i);
-		lp.push_back({{bs[0].l, true}, {bs[0].r, false}});
-		int a, b, m;
-		for(i = 1; i < n; i++) {
-			a = 0, b = ts.size()-1;
-			while(a < b) {
-				m = (a+b) / 2;
-				if(bs[i].i > ts[m]) b = m;
-				else a = m+1;
-			}
-			if(bs[i].i > ts[a]) {
-				ts[a] = bs[i].i;
-				lp[a].push_back({bs[i].l, true});
-				lp[a].push_back({bs[i].r, false});
-			} else {
-				ts.push_back(bs[i].i);
-				lp.push_back({{bs[i].l, true}, {bs[i].r, false}});
-			}
-		}
-		int op, last;
+		mi[1] = 0, ma[1] = 0;
 		int res = 0;
-		for(auto &ps : lp) {
-			sort(ps.begin(), ps.end(), compPos);
-			op = 1;
-			last = ps[0].x;
-			for(i = 1; i < ps.size(); i++) {
-				if(op > 0) res += ps[i].x - last;
-				last = ps[i].x;
-				if(ps[i].deb) op ++;
-				else op --;
-			}
-		}
+		while(n --> 0) {
+			cin >> l >> r >> h;
+			res += add(l, r, h, 1, 1, MAX);
+		};
 		cout << res << "\n";
 	}
 
+	cin >> c;
 	return 0;
 }
